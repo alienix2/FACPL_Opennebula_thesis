670ac4127187be2ba1b9c7eed3fbbfe69ecc7120
alienix2 <mattemoni@gmail.com>
2024-09-10 18:01:15 +0200

    Debugged successfully

--- resource_management/src/utilities/BaseCodeExecutor.java
+++ resource_management/src/utilities/BaseCodeExecutor.java
@@ -62,8 +62,6 @@ public class BaseCodeExecutor implements CodeExecutorInterface{
                 mainMethod.invoke(null, (Object) args);
                 logger.info("Successfully ran class: " + className);
             } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-            	e.printStackTrace();
-            	System.out.println(e.getCause().toString());
             	logger.severe("Error executing main method of class: " + className + " " + e.getMessage());
                 throw e;
             }
